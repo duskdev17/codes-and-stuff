d58fd145dc3bd22ced3156d3dc0d8856884b5472
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-06-04 00:09:03 +0600

    update

--- Algorithms/LinkedList/LinkedList.cpp
+++ Algorithms/LinkedList/LinkedList.cpp
@@ -1,4 +1,4 @@
-//Single LinkedList -
+//Single LinkedList - Delete Any (Cont.)
 #include<iostream>
 using namespace std;
 
@@ -6,3 +6,22 @@ struct node {
 	int value;
 	struct node* next;
 };
+struct node* head;
+
+void deleteAny(int x) {
+	if (head == NULL) //list empty 
+		return;
+	struct node* cur = head;
+	struct node* prev = NULL;
+	while (cur->value != x) {
+		prev = cur;
+		cur = cur->next;
+	}
+	if (prev != NULL)
+		prev->next = cur->next;
+	free(cur);
+}
+
+int main() {
+
+}
