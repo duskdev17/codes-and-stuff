1eb7c1906733afad920d9be13f75b03e7b51f9de
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-06-04 00:30:03 +0600

    Update LinkedList.cpp

--- Algorithms/LinkedList/LinkedList.cpp
+++ Algorithms/LinkedList/LinkedList.cpp
@@ -8,6 +8,17 @@ struct node {
 };
 struct node* head;
 
+void insertHead(int n) {
+	//create a new node 
+	struct node* newItem;
+	newItem = (struct node*)malloc(sizeof(struct node));
+	newItem->value = n;
+	newItem->next = NULL;
+	//insert the new node at the head 
+	newItem->next = head;
+	head = newItem;
+}
+
 void deleteAny(int x) {
 	if (head == NULL) //list empty 
 		return;
@@ -22,6 +33,38 @@ void deleteAny(int x) {
 	free(cur);
 }
 
+void printList()
+{
+	if (head == NULL) // no list at all 
+		return;
+	struct node* cur = head;
+	while (cur != NULL)
+	{
+		printf("%d \t", cur->value);
+		cur = cur->next;
+	}
+}
+
 int main() {
+	int n, value, delValue;
+
+	cout << "Enter how many elements you want to push to the list: ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> value;
+		insertHead(value);
+	}
+
+	cout << "Your list" << endl;
+	printList();
+
+	cout << "\nEnter a value to delte from the list: ";
+	cin >> delValue;
+	deleteAny(delValue);
+
+	cout << endl;
+	cout << "Your list after deletion: " << endl;
+	printList();
 
 }
