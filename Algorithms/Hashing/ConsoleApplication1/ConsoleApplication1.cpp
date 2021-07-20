6c53524df4a1f5b2c901cc4b2990851e4c20906d
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-07-20 10:43:42 +0600

    update

--- Algorithms/Hashing/ConsoleApplication1/ConsoleApplication1.cpp
+++ Algorithms/Hashing/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,32 +1,18 @@
 #include<iostream>
-#define SIZE 9
 using namespace std;
 
-int LinearSearch(int arr[], int key);
-
-int main() {	
-	int n, Arr[SIZE] = { 8,14,52,13,6,76,45,12, 7 };
-	int key= 19;
-
-	int pos = LinearSearch(Arr, key);
-
-	if (pos == -1) {
-		cout << "Key is not found in the array." << endl;
-	}
-	else if (pos == 1) {
-		cout << "Key found at " << key << "location" << endl;
-	}
-
+void domyhomework() {
+	cout << "I just got executed!" << endl;
 }
 
-int LinearSearch(int arr[], int sKey) {
-	int i;
-	for ( i = 0; i < SIZE; i++) {
-		if (arr[i] == sKey) {
-			return i;
-		}
-	}
-	return -i;
-
+int gotosleep() {
+	cout << "I just got executed!" << endl;
+	return 0;
 }
 
+
+int main() {
+	domyhomework(); // call the function
+	gotosleep();
+	return 0;
+}
