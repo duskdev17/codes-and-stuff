83ab331e00ea1c73b94ae84acfa0ba3512051952
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-09-03 19:59:55 +0600

    update

---
+++ Visual Studio C++/CSE 2106/Week 5, 6, 7/2. Arrays.cpp
@@ -0,0 +1,57 @@
+//Arrays
+//Write a c++ program that sorts a given array.
+#include<iostream>
+using namespace std;
+
+void beforeSort(int array[], int arrSize) {
+    cout << "Unsorted Array: ";
+
+    for (int i = 0; i < arrSize; i++) {
+        cout << array[i] << " ";
+    }
+
+    cout << endl;
+}
+
+void bubbleSort(int array[], int arrSize) {
+    int temp = 0;
+
+    for (int i = 0; i < arrSize; i++) {
+        for (int j = i + 1; j < arrSize; j++) {
+            if (array[j] < array[i]) {
+                //swap
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+void afterSort(int array[], int arrSize) {
+    cout << "Sorted Array: ";
+
+    for (int i = 0; i < arrSize; i++) {
+        cout << array[i] << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    int array[1000], n;
+
+    cout << "Enter number of elements to insert in the array: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> array[i];
+    }
+
+    beforeSort(array, n);
+    bubbleSort(array, n);
+    afterSort(array, n);
+
+    return 0;
+}
---
+++ Visual Studio C++/CSE 2106/Week 5, 6, 7/3. Returning Object.cpp
@@ -0,0 +1,37 @@
+//returning object
+//Write a c++ program to find the third angle of a triangle
+#include <iostream>
+using namespace std;
+
+class triangle {
+public:
+    double angle1, angle2, angle3;
+};
+
+triangle findAngle(double a, double b) {
+    triangle ob;
+
+    ob.angle1 = a;
+    ob.angle2 = b;
+
+    ob.angle3 = 180 - (ob.angle1 + ob.angle2);
+
+    cout << "Third Angle: " << ob.angle3 << endl;
+
+    return ob;
+}
+
+int main() {
+    double a, b;
+    triangle object;
+
+    cout << "Enter First Angle: ";
+    cin >> a;
+
+    cout << "Enter Second Angle: ";
+    cin >> b;
+
+    object = findAngle(a, b);
+
+    return 0;
+}
