00e533ffbe991c90e08d1de88d05c749b0126fb2
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-09-06 19:42:00 +0600

    Update URI Problems.cpp

--- Visual Studio C++/URI Problems/URI Problems/URI Problems.cpp
+++ Visual Studio C++/URI Problems/URI Problems/URI Problems.cpp
@@ -1,70 +1,47 @@
-// C++ Program to find root of a function, f(x)
+// C++ Program to find root of an equations using secant method
 #include<iostream>
 using namespace std;
 
-const int MAX_ITERATIONS = 10000;
-
-// Function to calculate f(x)
 float f(float x)
 {
-	// Taking f(x) = x ^ 3 + 2x ^ 2 + 10x - 20
-	return 1 * pow(x, 3) + 2 * x * x + 10 * x - 20;
+	// x^3+x-1
+	float f = pow(x, 3) + x - 1;
+	return f;
 }
 
-void Muller(float a, float b, float c)
+void secant(float x1, float x2, float E)
 {
-	int i;
-	float res;
+	float n = 0, xm, x0, c;
+	if (f(x1) * f(x2) < 0) {
+		do {
+			x0 = (x1 * f(x2) - x2 * f(x1)) / (f(x2) - f(x1));
+			c = f(x1) * f(x0);
+			x1 = x2;
+			x2 = x0;
+			n++;
+			if (c == 0)
+				break;
+			xm = (x1 * f(x2) - x2 * f(x1)) / (f(x2) - f(x1));
+		} while (fabs(xm - x0) >= E); 
+								
 
-	for (i = 0;; ++i)
-	{
-		// Calculating various constants required
-		// to calculate x3
-		float f1 = f(a);
-		float f2 = f(b);
-		float f3 = f(c);
-		float d1 = f1 - f3;
-		float d2 = f2 - f3;
-		float h1 = a - c;
-		float h2 = b - c;
-		float a0 = f3;
-		float a1 = (((d2 * pow(h1, 2)) - (d1 * pow(h2, 2)))	/ ((h1 * h2) * (h1 - h2)));
-		float a2 = (((d1 * h2) - (d2 * h1)) / ((h1 * h2) * (h1 - h2)));
-		float x = ((-2 * a0) / (a1 + abs(sqrt(a1 * a1 - 4 * a0 * a2))));
-		float y = ((-2 * a0) / (a1 - abs(sqrt(a1 * a1 - 4 * a0 * a2))));
-
-		// Taking the root which is closer to x2
-		if (x >= y)
-			res = x + c;
-		else
-			res = y + c;
-
-		// checking for resemblance of x3 with x2 till
-		// two decimal places
-		float m = res * 100;
-		float n = c * 100;
-		m = floor(m);
-		n = floor(n);
-		if (m == n)
-			break;
-		a = b;
-		b = c;
-		c = res;
-		if (i > MAX_ITERATIONS)
-		{
-			cout << "Root cannot be found using"
-				<< " Muller's method";
-			break;
-		}
+		cout << "Root of the given equation=" << x0 << endl;
+		cout << "No. of iterations = " << n << endl;
 	}
-	if (i <= MAX_ITERATIONS)
-		cout << "The value of the root is " << res;
+	else
+		cout << "Can not find a root in the given inteval";
 }
 
-// Driver main function
+
 int main()
 {
-	float a = 0, b = 1, c = 2;
-	Muller(a, b, c);
+	int x1, x2, E = 0.0001;
+
+	cout << "Enter First Value:";
+	cin >> x1;
+	cout << "Enter Second Value:";
+	cin >> x2;
+
+	secant(x1, x2, E);
 	return 0;
 }
