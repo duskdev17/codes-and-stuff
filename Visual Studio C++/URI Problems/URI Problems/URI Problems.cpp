98c054976435ee4fdebd5a11de04a7f9643e50ee
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-09-06 11:02:46 +0600

    Update URI Problems.cpp

--- Visual Studio C++/URI Problems/URI Problems/URI Problems.cpp
+++ Visual Studio C++/URI Problems/URI Problems/URI Problems.cpp
@@ -1,64 +1,71 @@
-//combined code - array, nested class, static class members, pointer
-//write a c++ program that sorts an array in ascending order.
+//#include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
 
-class Class {
-public:
-    class nestedClass {
-    public:
-        static void takeInput(int* myArr, int n) {
-            for (int i = 0; i < n; i++) {
-                cin >> myArr[i];
-            }
+const int MAX_ITERATIONS = 10000;
+
+// Function to calculate f(x)
+float f(float x)
+{
+    // Taking f(x) = x ^ 3 + 2x ^ 2 + 10x - 20
+    return 1 * pow(x, 3) + 2 * x * x + 10 * x - 20;
+}
+
+void Muller(float a, float b, float c)
+{
+    int i;
+    float res;
+
+    for (i = 0;; ++i)
+    {
+        // Calculating various constants required
+        // to calculate x3
+        float f1 = f(a);
+        float f2 = f(b);
+        float f3 = f(c);
+        float d1 = f1 - f3;
+        float d2 = f2 - f3;
+        float h1 = a - c;
+        float h2 = b - c;
+        float a0 = f3;
+        float a1 = (((d2 * pow(h1, 2)) - (d1 * pow(h2, 2)))
+            / ((h1 * h2) * (h1 - h2)));
+        float a2 = (((d1 * h2) - (d2 * h1)) / ((h1 * h2) * (h1 - h2)));
+        float x = ((-2 * a0) / (a1 + abs(sqrt(a1 * a1 - 4 * a0 * a2))));
+        float y = ((-2 * a0) / (a1 - abs(sqrt(a1 * a1 - 4 * a0 * a2))));
+
+        // Taking the root which is closer to x2
+        if (x >= y)
+            res = x + c;
+        else
+            res = y + c;
+
+        // checking for resemblance of x3 with x2 till
+        // two decimal places
+        float m = res * 100;
+        float n = c * 100;
+        m = floor(m);
+        n = floor(n);
+        if (m == n)
+            break;
+        a = b;
+        b = c;
+        c = res;
+        if (i > MAX_ITERATIONS)
+        {
+            cout << "Root cannot be found using"
+                << " Muller's method";
+            break;
         }
-
-        static void displayUnsorted(int* myArr, int n) {
-            cout << "Unsorted Array: ";
-            for (int i = 0; i < n; i++)
-                cout << myArr[i] << " ";
-            cout << endl;
-        }
-
-        void insertionSort(int* myArr, int n) {
-            int temp, j;
-            for (int i = 1; i < n; i++) {
-                temp = myArr[i];
-                j = i - 1;
-                while (j >= 0 && myArr[j] > temp) {
-                    myArr[j + 1] = myArr[j];
-                    j = j - 1;
-                }
-                myArr[j + 1] = temp;
-            }
-        }
-
-        void displaySorted(int* myArr, int n) {
-            cout << "Sorted Array: \t";
-
-            for (int i = 0; i < n; i++) {
-                cout << myArr[i] << " ";
-            }
-            cout << endl;
-        }
-    };
+    }
+    if (i <= MAX_ITERATIONS)
+        cout << "The value of the root is " << res << endl;
+}
 
 
-};
-
-int main() {
-    int n, array[1000];
-
-    cout << "Enter number of inputs: ";
-    cin >> n;
-
-    Class::nestedClass obj;
-
-    Class::nestedClass::takeInput(array, n);
-    Class::nestedClass::displayUnsorted(array, n);
-
-    obj.insertionSort(array, n);
-    obj.displaySorted(array, n);
-
+int main()
+{
+    float a = 0, b = 1, c = 2;
+    Muller(a, b, c);
     return 0;
 }
