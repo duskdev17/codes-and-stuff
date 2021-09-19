7388aa38a9d5b44c9d8103105b8fc9c183181775
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-09-19 09:08:12 +0600

    update

--- class and lab codes/class and lab codes.cpp
+++ class and lab codes/class and lab codes.cpp
@@ -1,35 +1,94 @@
-﻿#include <iostream>
+﻿
+#include<iostream>
+#include<string.h>
 using namespace std;
-
-class base1 {
-    int i = 100;
-
-public:
-    void showi(void)
-    {
-        cout << i << endl;
-    }
-
-};
-
-class base2 {
-    int j = 200;
-public:
-    void showj(void)
-    {
-        cout << j << endl;
-    }
-};
-
-
-class derived : public base1, public base2 {
-
-};
+void showmenu()
+{
+    cout << "********MENU********" << endl;
+    cout << "1.Check balance" << endl;
+    cout << "2.Deposit" << endl;
+    cout << "3.Withdraw" << endl;
+    cout << "4.Send money on BKASH/NAGAD" << endl;
+    cout << "5.Exit" << endl;
+    cout << "********END********" << endl;
+}
 
 int main()
 {
-    derived d;
-    d.showi();
-    d.showj();
-    return 0;
+    int option;
+    int password;
+    double balance = 5000;
+    cout << "Please input your pin: ";
+    cin >> password;
+    do
+    {
+        if (password == 1234)
+        {
+            showmenu();
+        }
+        else
+        {
+            cout << "Enter the correct pin: ";
+            system("PAUSE");
+            system("CLS");
+
+        }
+
+        cout << "Please Choose An Option: ";
+
+        cin >> option;
+        system("cls");
+        switch (option)
+        {
+        case 1:
+        {
+            cout << "Balance is: " << balance << "TK" << endl;
+            break;
+        }
+        case 2:
+        {
+            cout << "Deposit Amount: ";
+            double depositamount;
+            cin >> depositamount;
+            balance += depositamount;
+            break;
+        }
+        case 3:
+        {
+            cout << "Withdraw Amount: ";
+            double withdrawamount;
+            cin >> withdrawamount;
+            if (withdrawamount <= balance)
+            {
+                balance -= withdrawamount;
+            }
+            else
+                cout << "Not enough money" << endl;
+            break;
+        }
+        case 4:
+        {
+            cout << "Give The BKASH/NAGAD NUMBER: ";
+            double pnum;
+            cin >> pnum;
+            cout << "Send money amount :";
+            double sndmny;
+            cin >> sndmny;
+            if (sndmny <= balance)
+            {
+                balance -= sndmny;
+                cout << "SUCCESSFULLY SENT" << endl;
+
+            }
+            else
+                cout << "Not enough money" << endl;
+            break;
+        }
+
+        }
+
+    } while (option != 5);
+
+    system("pause>0");
+
 }
