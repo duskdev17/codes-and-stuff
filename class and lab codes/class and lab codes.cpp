410ebbe68474e97c85af1e019071e955644e8b67
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-10-03 14:44:48 +0600

    Update class and lab codes.cpp

--- class and lab codes/class and lab codes.cpp
+++ class and lab codes/class and lab codes.cpp
@@ -1,1 +1,23 @@
-﻿
+﻿#include<iostream>
+using namespace std;
+
+void H
+
+int main() {
+	int i, n, A[1000];
+
+	cout << "Input count: ";
+	cin >> n;
+	
+	cout << "Enter data: ";
+	for (int i = 0; i < n; i++) {
+		cin >> A[i];
+	}
+
+	HEAPSORT(A, n);
+
+	cout << "Sorted Sequence is: ";
+	for (int i = 0; i < n; i++) {
+		cout << A[i] << " ";
+	}
+}
