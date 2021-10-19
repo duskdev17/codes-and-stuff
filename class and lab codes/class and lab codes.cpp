dd9bfd2fd4e5614e1a955c4d23d289e919b43d63
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-10-20 00:06:39 +0600

    Update class and lab codes.cpp

--- class and lab codes/class and lab codes.cpp
+++ class and lab codes/class and lab codes.cpp
@@ -1,25 +1,44 @@
-﻿//linear
-#include<iostream>
+﻿#include < iostream >
 using namespace std;
 
+int binarySearch(int arr[], int left, int right, int x) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == x) {
+            return mid;
+        }
+        else if (arr[mid] < x) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
 int main() {
-	int key, i=0,n, a[1000];
+    int myarr[10];
+    int num;
+    int output;
 
-	cout << "Enter number of inputs: ";
-	cin >> n;
+    cout << "Please enter 10 elements ASCENDING order" << endl;
+    for (int i = 0; i < 10; i++) {
+        cin >> myarr[i];
+    }
+    cout << "Please enter an element to search" << endl;
+    cin >> num;
 
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
+    output = binarySearch(myarr, 0, 9, num);
 
-	cout << "Enter a number to search: ";
-	cin >> key;
+    if (output == -1) {
+        cout << "No Match Found" << endl;
+    }
+    else {
+        cout << "Match found at position: " << output << endl;
+    }
 
-	while (i<n && a[i] != key) {
-		i++;
-	}
-
-	cout << "Item found at " << i << " index." << endl;
-
-	return 0;
+    return 0;
 }
