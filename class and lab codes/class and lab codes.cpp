9a34eacab8682698a63f10289cd3bd9a853c9d4e
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-10-19 22:13:00 +0600

    Update class and lab codes.cpp

--- class and lab codes/class and lab codes.cpp
+++ class and lab codes/class and lab codes.cpp
@@ -1,2 +1,25 @@
 ﻿//linear
 #include<iostream>
+using namespace std;
+
+int main() {
+	int key, i=0,n, a[1000];
+
+	cout << "Enter number of inputs: ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	cout << "Enter a number to search: ";
+	cin >> key;
+
+	while (i<n && a[i] != key) {
+		i++;
+	}
+
+	cout << "Item found at " << i << " index." << endl;
+
+	return 0;
+}
