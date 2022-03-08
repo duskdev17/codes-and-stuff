47c3528968d6b15c0cc3be18aea6dbac370c9b84
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-03-08 18:35:36 +0600

    update

---
+++ class and lab codes/CSE 3102/untitled0.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 08:42:29 2022
+
+@author: Student
+"""
+
+#module
+import random
+print(random.randrange(1, 10))
---
+++ class and lab codes/CSE 3102/untitled1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 08:52:56 2022
+
+@author: Student
+"""
+
+#list
+list1=[1,2,3,4,5,6,7,8,9,10]
+print(list1)
+print(list1[-1][0])
